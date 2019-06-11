1039284834a4a140a1a3fb855087c0b8ce7f9886
当轩 <jinxin.sjx@alibaba-inc.com>
2019-06-11 23:24:00 +0800

    inital commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,187 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div v-if="enabled">
+    <div class="weicomment-container" v-view="viewHandler">
+      <div v-if="!(comments.length > 0)" class="spinner">
+        <div class="cube1"></div>
+        <div class="cube2"></div>
+      </div>
+      <div class="list" v-else>
+        <div class="header">
+          评论 <span class="count">{{comments.length}}</span>
+          <a class="weibo-link" target="_black" :href="'https://m.weibo.cn/detail/' + weiboId">
+            去评论 >
+          </a>
+        </div>
+        <div class="comment-item" :key="item.id" v-for="item in comments">
+          <img class="profile" :src="item.user.profile_image_url">
+          <span class="nickname">
+            {{item.user.screen_name}}
+          </span>
+          <span class="time">
+            {{item.created_at}}
+          </span>
+          <div class="text">
+            {{item.text}}
+          </div>
+        </div>
+        <div class="footer">
+          Powered By @WeiComment
+        </div>
+      </div>
+    </div>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import dayjs from 'dayjs';
 
 export default {
   name: 'app',
+  data: () => {
+    return {
+      rendered: false,
+      commentsData: [],
+      weiboId: null,
+      enabled: true,
+    }
+  },
+  computed: {
+    comments() {
+      return this.commentsData.map(item => {
+        return {
+          ...item,
+          created_at: dayjs(item.created_at).format('YYYY-MM-DD HH:mm:ss')
+        }
+      });
+    }
+  },
   components: {
-    HelloWorld
+  },
+  methods: {
+    async viewHandler(e) {
+      if (e.type === 'enter' && !this.rendered) {
+        this.rendered = true;
+
+        const weiCommentId = document.querySelector('[data-weicomment-id]').getAttribute('data-weicomment-id');
+        if (weiCommentId) {
+          this.weiboId = weiCommentId;
+        } else {
+          this.enabled = false;
+        }
+        
+
+        const res = await fetch(`https://cors-anywhere.herokuapp.com/https://m.weibo.cn/comments/hotflow?id=${weiCommentId}&mid=${weiCommentId}&max_id_type=0`)
+        const json = await res.json();
+        this.commentsData = json.data.data.concat(json.data.data).concat(json.data.data).concat(json.data.data).concat(json.data.data).concat(json.data.data).concat(json.data.data);
+      }
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="less" scoped>
+.weicomment-container {
+  padding: 10px 20px;
+  position: relative;
+
+  .list {
+  }
+
+  .comment-item {
+    border-top: 1px solid #DEDEDE;
+    padding: 15px 0px;
+  }
+  .profile {
+    width: 20px;
+    vertical-align: middle;
+  }
+  .nickname {
+    font-size: 14px;
+    vertical-align: middle;
+    font-weight: bold;
+  }
+  .text {
+    font-size: 16px;
+    line-height: 24px;
+    padding: 10px 0px;
+  }
+  .time {
+    color: gray;
+    font-size: 12px;
+  }
+
+  .footer {
+    border-top: 1px solid #DEDEDE;
+    color: gray;
+    text-align: right;
+  }
+
+  .header {
+    font-size: 16px;
+    margin-bottom: 5px;
+    position: relative;
+
+    .count {
+      margin-left: 2px;
+    }
+
+    .weibo-link {
+      color: #e6152d;
+      position: absolute;
+      right: 10px;
+    }
+  }
 }
+
+.spinner {
+  margin: 100px auto;
+  width: 40px;
+  height: 40px;
+  position: relative;
+}
+
+.cube1, .cube2 {
+  background-color: #333;
+  width: 15px;
+  height: 15px;
+  position: absolute;
+  top: 0;
+  left: 0;
+  
+  -webkit-animation: sk-cubemove 1.8s infinite ease-in-out;
+  animation: sk-cubemove 1.8s infinite ease-in-out;
+}
+
+.cube2 {
+  -webkit-animation-delay: -0.9s;
+  animation-delay: -0.9s;
+}
+
+@-webkit-keyframes sk-cubemove {
+  25% { -webkit-transform: translateX(42px) rotate(-90deg) scale(0.5) }
+  50% { -webkit-transform: translateX(42px) translateY(42px) rotate(-180deg) }
+  75% { -webkit-transform: translateX(0px) translateY(42px) rotate(-270deg) scale(0.5) }
+  100% { -webkit-transform: rotate(-360deg) }
+}
+
+@keyframes sk-cubemove {
+  25% { 
+    transform: translateX(42px) rotate(-90deg) scale(0.5);
+    -webkit-transform: translateX(42px) rotate(-90deg) scale(0.5);
+  } 50% { 
+    transform: translateX(42px) translateY(42px) rotate(-179deg);
+    -webkit-transform: translateX(42px) translateY(42px) rotate(-179deg);
+  } 50.1% { 
+    transform: translateX(42px) translateY(42px) rotate(-180deg);
+    -webkit-transform: translateX(42px) translateY(42px) rotate(-180deg);
+  } 75% { 
+    transform: translateX(0px) translateY(42px) rotate(-270deg) scale(0.5);
+    -webkit-transform: translateX(0px) translateY(42px) rotate(-270deg) scale(0.5);
+  } 100% { 
+    transform: rotate(-360deg);
+    -webkit-transform: rotate(-360deg);
+  }
+}
+
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import checkView from 'vue-check-view'
+
 
 Vue.config.productionTip = false
+
+Vue.use(checkView);
 
 new Vue({
   render: h => h(App),
